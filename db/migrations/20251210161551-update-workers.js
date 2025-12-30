2553ff48e2f31921e65bfdbb13743636ebeeff7c
ruffaaw <rafal.p.ciupek@gmail.com>
2025-12-30 13:57:29 +0100

    feat: added migration

--- db/migrations/20251210161551-update-workers.js
+++ db/migrations/20251210161551-update-workers.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("workers", "workerType", {
-      type: Sequelize.ENUM("0", "1"),
+      type: Sequelize.ENUM("0", "1", "2"),
     });
   },
 
---
+++ utils/status.js
@@ -0,0 +1,11 @@
+export const activeStatusByType = {
+  0: "W trakcie sprzątania",
+  1: "W trakcie serwisu",
+  2: "W trakcie kontroli",
+};
+
+export const finishedStatusByType = {
+  0: "Sprzątanie zakończone",
+  1: "Serwis zakończony",
+  2: "Kontrola zakończona",
+};
